fa5c4b9e2de8377b21c4da6adc9212c0628e6012
Muhammad Gilang Nur Haliz <100754364+mgilangnurhlz@users.noreply.github.com>
2023-01-29 09:52:10 +0700

    Add files via upload

---
+++ nvim/lua/colors-config/nightfox.lua
@@ -0,0 +1,12 @@
+local nightfox = require('nightfox')
+
+nightfox.setup({
+    fox = 'Nightfox', -- change the colorscheme
+    styles = {
+        comments = 'italic',
+        functions = 'italic',
+    },
+})
+
+-- load the configurations
+nightfox.load()
